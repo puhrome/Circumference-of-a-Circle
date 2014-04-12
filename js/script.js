3ae981a2b0bcc2491be69835331864caffd171b2
puhrome <phirom.yim@fullsail.edu>
2014-04-11 21:59:16 -0400

    Circumference of a Circle
    Using WebStorm to write expression for the circumference of a circle
    with HTML and JS.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/**
+ * The Circumference of a Circle Created by Phirom Yim 4/11/14.
+ */
+var radius;
+radius = 6;
+const PI = 3.1415;
+//multiply 2, PI and the radius and assign to circle variable
+var circumference = radius * 2 * PI;
+console.log(circumference);
+alert(circumference)
